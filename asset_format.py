6aae0e576d529c59d31c41557c590202bc34243c
Tilapiatsu <tilapiatsu@hotmail.fr>
2019-04-28 21:04:01 +0200

    + create  class to handle asset handling more easily

---
+++ asset_format.py
@@ -0,0 +1,17 @@
+import bpy
+
+class AssetFormat(object):
+    def __init__(self, mesh, texture):
+        pass
+
+    def import_asset(self):
+        pass
+    
+    def update_asset(self):
+        pass
+
+    def import_texture(self):
+        pass
+    
+    def update_texture(self):
+        pass
