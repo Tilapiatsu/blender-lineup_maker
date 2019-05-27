f61da377b27ed0aebca7bfb5f41858093ef66799
Tilapiatsu <tilapiatsu@hotmail.fr>
2019-05-28 00:47:48 +0200

    * saving file is now working for animation, rendering and compositing

--- OP_main.py
+++ OP_main.py
@@ -105,26 +105,43 @@ class LM_OP_RenderAssets(bpy.types.Operator):
 	remaining_frames = None
 	remaining_assets = None
 	rendering = None
-	path = "//"
 	disablerbbutton = False
 	need_render_asset = None
 	asset_number = 0
+	composite_node = None
+	output_node = None
+	context = None
+	initial_view_layer = None
 
 	def pre(self, dummy):
-		print('PRE')
 		self.rendering = True
-
+		self.report({'INFO'}, "Lineup Maker : Rendering '{}'".format(os.path.join(self.need_render_asset[0].render_path, self.need_render_asset[0].name)))
+		
 	def post(self, dummy):
-		print('POST')
-		if self.remaining_frames <= 0:
-			self.need_render_asset.pop(0)
-			self.remaining_frames = self.frame_range
-			# self.shots.pop(0)
-		self.remaining_frames -= 1
-		self.rendering = False
+		if self.remaining_frames <= 1:
+			asset = self.need_render_asset[0]
+
+			self.composite_node = self.build_composite_nodegraph(self.context, self.asset_number, asset)
+
+			asset.need_render = False
+			asset.rendered = True
+		else:
+			self.remaining_frames -= 1
 
 	def cancelled(self, dummy):
 		self.stop = True
+
+	def register_render_handler(self):
+		bpy.app.handlers.render_pre.append(self.pre)
+		bpy.app.handlers.render_post.append(self.post)
+		bpy.app.handlers.render_cancel.append(self.cancelled)
+		self._timer = bpy.context.window_manager.event_timer_add(0.5, window=bpy.context.window)
+
+	def unregister_render_handler(self):
+		bpy.app.handlers.render_pre.remove(self.pre)
+		bpy.app.handlers.render_post.remove(self.post)
+		bpy.app.handlers.render_cancel.remove(self.cancelled)
+		bpy.context.window_manager.event_timer_remove(self._timer)
 
 	def execute(self, context):
 		self.stop = False
@@ -160,20 +177,18 @@ class LM_OP_RenderAssets(bpy.types.Operator):
 				asset.need_render = True
 		
 		self.need_render_asset = [a for a in scene.lm_asset_list if a.need_render]
-
 		self.remaining_assets = len(self.need_render_asset)
-
 		self.asset_number = 0
+		self.initial_view_layer = context.window.view_layer
 
 		self.frame_range = self.get_current_frame_range(context)
 		self.remaining_frames = self.frame_range
 		
+		self.clear_composite_tree(context)
 
-		bpy.app.handlers.render_pre.append(self.pre)
-		bpy.app.handlers.render_post.append(self.post)
-		bpy.app.handlers.render_cancel.append(self.cancelled)
+		self.register_render_handler()
 
-		self._timer = bpy.context.window_manager.event_timer_add(0.5, window=bpy.context.window)
+		
 		bpy.context.window_manager.modal_handler_add(self)
 
 		return {"RUNNING_MODAL"}
@@ -183,12 +198,34 @@ class LM_OP_RenderAssets(bpy.types.Operator):
 
 			if True in (not self.need_render_asset, self.stop is True): 
 
-				bpy.app.handlers.render_pre.remove(self.pre)
-				bpy.app.handlers.render_post.remove(self.post)
-				bpy.app.handlers.render_cancel.remove(self.cancelled)
+				self.unregister_render_handler()
 				bpy.context.window_manager.event_timer_remove(self._timer)
 
+				context.window.view_layer = self.initial_view_layer
+
+				if self.stop:
+					self.report({'WARNING'}, "Lineup Maker : Render cancelled by user")
+				else:
+					self.report({'INFO'}, "Lineup Maker : Render completed")
+
 				return {"FINISHED"} 
+
+			elif self.rendering and self.remaining_assets and self.composite_node is not None:
+				self.unregister_render_handler()
+				print('Lineup Maker : Rendering composited image of "{}"'.format(self.need_render_asset[0].name))
+				
+				bpy.ops.render.render(write_still=False)
+				self.composite_node.mute = True
+				
+				self.composite_node = None
+				self.need_render_asset.pop(0)
+				self.remaining_frames = self.frame_range
+
+				self.remaining_assets -= 1
+				self.asset_number += 1
+
+				self.rendering = False
+				self.register_render_handler()
 
 			elif self.rendering is False: 
 				self.render(context)
@@ -200,11 +237,6 @@ class LM_OP_RenderAssets(bpy.types.Operator):
 		scn.camera = bpy.data.objects[self.shots[0]] 	
 
 		scn.render.film_transparent = True
-
-		
-		self.clear_composite_tree(context)
-
-		composite_nodes = {}
 
 		# context.window_manager.progress_begin(0,len(need_render_asset))
 		# self.remaining_assets = len(self.need_render_asset)
@@ -218,97 +250,13 @@ class LM_OP_RenderAssets(bpy.types.Operator):
 		# switch to the proper view_layer
 		context.window.view_layer = scn.view_layers[asset.name]
 
-		output_node = self.build_output_nodegraph(context, self.asset_number, asset)
-
+		self.output_node = self.build_output_nodegraph(context, self.asset_number, asset)
 		bpy.context.scene.render.filepath = render_filename + self.shots[0] + '_'
+		self.output_node.mute = True
 
-		bpy.ops.render.render("INVOKE_DEFAULT", animation=True, write_still=True, layer=asset.view_layer)
+		self.context = context
 
-		asset.need_render = False
-		asset.rendered = True
-		
-		output_node.mute = True
-
-		composite_nodes[asset.name] = self.build_composite_nodegraph(context, self.asset_number, asset)
-		# context.window_manager.progress_update(i)
-
-		self.remaining_assets -= 1
-		self.asset_number += 1
-
-	def render_bak(self, context):
-		context.scene.render.film_transparent = True
-
-		for asset in context.scene.lm_asset_list:
-			render_path, render_filename = self.get_render_path(context, asset.name)
-
-			# Set  need_render status for each assets
-			need_render = True
-			if not context.scene.lm_force_render:
-				if asset.render_date:
-					need_render = False
-					rendered_files = os.listdir(render_path)
-					if len(rendered_files) < self.get_current_frame_range():
-						need_render = True
-					else:
-						for f in os.listdir(render_path):
-							if asset.render_date < asset.import_date:
-								need_render = True
-								break
-				else:
-					asset.need_render = True
-			else:
-				asset.need_render = True
-		
-		need_render_asset = [a for a in context.scene.lm_asset_list if a.need_render]
-		
-		self.clear_composite_tree(context)
-
-		composite_nodes = {}
-
-		context.window_manager.progress_begin(0,len(need_render_asset))
-
-		for i, asset in enumerate(need_render_asset):
-			
-			render_path, render_filename = self.get_render_path(context, asset.name)
-
-			asset.need_render = True
-			asset.render_path = render_path
-
-			# switch to the proper view_layer
-			context.window.view_layer = context.scene.view_layers[asset.name]
-
-			output_node = self.build_output_nodegraph(context, i, asset)
-			
-			for frame in range(context.scene.frame_start, context.scene.frame_end + 1):
-				print('Lineup Maker : Rendering asset "{}" | Frame {}' .format(asset.view_layer, str(frame).zfill(4)))
-				self.report({'INFO'}, 'Lineup Maker : Rendering asset "{}" | Frame {}' .format(asset.view_layer, str(frame).zfill(4)))
-				context.scene.frame_set(frame)
-				
-				bpy.context.scene.render.filepath = render_filename + str(frame).zfill(4)
-				bpy.ops.render.render("INVOKE_DEFAULT", write_still=False, layer=asset.view_layer)
-				# bpy.ops.wm.redraw_timer(type='DRAW_WIN_SWAP', iterations=1)
-
-				asset.need_render = False
-				asset.rendered = True
-			
-			output_node.mute = True
-
-			composite_nodes[asset.name] = self.build_composite_nodegraph(context, i, asset)
-			context.window_manager.progress_update(i)
-
-		for node in composite_nodes.values():
-			node.mute = False
-		
-		bpy.context.scene.render.filepath = render_filename
-		bpy.ops.render.render(write_still=False)
-
-
-		self.revert_need_render(context)
-
-		# Set the global View_layer active
-		context.window.view_layer = context.scene.view_layers[context.scene.lm_initial_view_layer]
-		context.window_manager.progress_end()
-		return {'FINISHED'}
+		bpy.ops.render.render("INVOKE_DEFAULT", animation=True, write_still=False, layer=asset.view_layer)
 
 	def isolate_collection_visibility(self, context, collections):
 		for asset in context.scene.lm_asset_list:
@@ -330,9 +278,8 @@ class LM_OP_RenderAssets(bpy.types.Operator):
 		tree = context.scene.node_tree
 		nodes = tree.nodes
 
-		location = (0, -1000 * index)
+		location = (0, -500 * index)
 		incr = 300
-
 
 		rl = nodes.new('CompositorNodeRLayers')
 		rl.location = location
@@ -355,6 +302,7 @@ class LM_OP_RenderAssets(bpy.types.Operator):
 		return out
 
 	def build_composite_nodegraph(self, context, index, asset):
+		print('Lineup Maker : Generating composite nodegraph')
 		tree = context.scene.node_tree
 		nodes = tree.nodes
 
@@ -410,7 +358,7 @@ class LM_OP_RenderAssets(bpy.types.Operator):
 		out.location = (location[0] + incr, location[1])
 		out.file_slots[0].path = asset.name + '_composite_'
 		out.base_path = path.abspath(path.join(asset.render_path, os.pardir))
-		out.mute = True
+		# out.mute = True
 
 		if mix:
 			tree.links.new(mix.outputs[0], out.inputs[0])
@@ -420,7 +368,6 @@ class LM_OP_RenderAssets(bpy.types.Operator):
 		asset.rendered = False
 		
 		return out
-
 
 	def get_composite_resolution(self, context):
 		fc = context.scene.frame_end-context.scene.frame_start
@@ -432,7 +379,6 @@ class LM_OP_RenderAssets(bpy.types.Operator):
 		y = math.floor(fc/2) * res_y + margin
 
 		return (x, y, margin)
-		
 
 	def get_render_path(self, context, asset_name):
 		render_path = os.path.abspath(os.path.join(os.path.abspath(context.scene.lm_render_path), asset_name))
