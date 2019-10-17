d0c11200a32452d15b18c59729c625058320d69d
Tilapiatsu <tilapiatsu@hotmail.fr>
2019-10-17 09:11:41 +0200

    * handling file acces denied on delete folder

--- helper.py
+++ helper.py
@@ -1,4 +1,4 @@
-import bpy, os, shutil
+import bpy, os, shutil, stat
 from . import variables as V
 
 def create_folder_if_neeed(path):
@@ -7,7 +7,17 @@ def create_folder_if_neeed(path):
 
 def delete_folder_if_exist(path):
 	if os.path.exists(path):
-		shutil.rmtree(path)
+		shutil.rmtree(path, onerror=file_acces_handler)
+
+def file_acces_handler(func, path, exc_info):
+    print('Handling Error for file ' , path)
+    print(exc_info)
+    # Check if file access issue
+    if not os.access(path, os.W_OK):
+       # Try to change the permision of file
+       os.chmod(path, stat.S_IWUSR)
+       # call the calling function again
+       func(path)
 
 def get_layer_collection(layer_collection, collection_name):
 	found = None
