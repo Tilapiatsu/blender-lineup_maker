d3e83a55b668f348255ed6e44ce0ba0d3284918b
Tilapiatsu <tilapiatsu@hotmail.fr>
2022-03-28 23:05:41 +0200

    * Fix dependency Setup

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import sys
 
 from os import path
 
-dependencies = ['pillow', 'fpdf']
+dependencies = ['Pillow', 'fpdf']
 
 def install_dependencies():
     current_dir = path.dirname(path.realpath(__file__))
     LineupMaker_dependencies_path = path.join(current_dir, 'LineupMakerDependencies')
     if LineupMaker_dependencies_path not in sys.path: sys.path.append(LineupMaker_dependencies_path)
 
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', *dependencies, '--target', LineupMaker_dependencies_path])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-U', *dependencies, '--target', LineupMaker_dependencies_path])
