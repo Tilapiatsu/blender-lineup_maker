fe7934c51fa8d6f736d2e75e922af14f84a7bf4c
Tilapiatsu <tilapiatsu@hotmail.fr>
2022-04-30 15:35:14 +0200

    * Fix Dependency Setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import subprocess
 import sys
+from . import helper as H
 
-from os import path
-from . import variable as V
 
-def install_dependencies():
+def install_dependencies(V):
+    H.create_folder_if_neeed(V.LM_DEPENDENCIES_PATH)
     if V.LM_DEPENDENCIES_PATH not in sys.path: sys.path.append(V.LM_DEPENDENCIES_PATH)
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-U', *V.LM_DEPENDENCIES, '--target', V.LM_DEPENDENCIES_PATH])
